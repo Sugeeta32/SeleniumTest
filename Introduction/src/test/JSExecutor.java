bb7fe2f8cc92d895348db201f2225b53367c197e
Sugeeta32 <sugeeta.gupta@gmail.com>
2020-05-04 18:02:47 -0400

    while loop

--- Introduction/src/test/JSExecutor.java
+++ Introduction/src/test/JSExecutor.java
@@ -8,22 +8,26 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class JSExecutor {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.homedepot.com/");
 		driver.findElement(By.id("headerSearch")).sendKeys("Ref");
-		driver.findElement(By.id("headerSearch")).sendKeys(Keys.DOWN);
-		driver.findElement(By.id("headerSearch")).sendKeys(Keys.DOWN);
-		//driver.findElement(By.id("headerSearch")).sendKeys(Keys.DOWN);
-		//driver.findElement(By.id("headerSearch")).sendKeys(Keys.DOWN);
-		//driver.findElement(By.id("headerSearch")).sendKeys(Keys.DOWN);
+		Thread.sleep(2000);
 		
+		driver.findElement(By.id("headerSearch")).sendKeys(Keys.DOWN);
+		 //System.out.println("print1 "+driver.findElement(By.id("headerSearch")).getText());
 		JavascriptExecutor js = (JavascriptExecutor)driver;
 		String text= "return document.getElementById(\"headerSearch\").value;";
-	String abc=(String) js.executeScript(text);
-	System.out.println(abc);
+		String abc=(String) js.executeScript(text);
+		//System.out.println("print abc= " + abc);
+		while(!abc.equals("refrigerator small")) {
+			//System.out.println("inside while loop text= " + text);
+			driver.findElement(By.id("headerSearch")).sendKeys(Keys.DOWN);
+			abc=(String) js.executeScript(text);
+			System.out.println("inside while loop " + abc);
+		}
 	}
 
 }
