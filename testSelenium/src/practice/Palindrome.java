563587056f76e8dbe940297fbe11e794fddcff1b
Sugeeta32 <sugeeta.gupta@gmail.com>
2020-05-18 14:04:54 -0400

    Interface

--- testSelenium/src/practice/Palindrome.java
+++ testSelenium/src/practice/Palindrome.java
@@ -1,8 +1,52 @@
 package practice;
 
-public class Palindrome {
+public class Palindrome implements Calculation {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculation a = new Palindrome();
+		
+		a.doPalindrome();
+		a.numberAdd();
+		
+//		Palindrome b = new Palindrome();
+//		b.doPalindrome();
+//		b.test();
+		
+	}
+	private void test() {
+		System.out.println("hi");
+	}
+	
+	@Override
+	public void doPalindrome() {
+		String s = "racecar  ";
+		String t = "";
+		for (int i = s.length() - 1; i >= 0; i--) {
+			t = t + s.charAt(i);
+		}
+		System.out.println(t);
+		
+	}
+	
+	@Override
+	public void numberAdd() {
+		for (int i = 20; i >= 10; i--) {
+			System.out.println(i);
+
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void numberSubtract() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void numberMultiply() {
 		// TODO Auto-generated method stub
 
 	}
